8a4a6199fe3ffba202f66806c7fae7a01cbbfaca
BaimZ <go-yura@bk.ru>
2023-12-12 21:27:43 +0300

    hz

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -1,7 +1,41 @@
 package auth
 
-import ssov1 "github.com/JustSkiv/protos/gen/go/sso"
+import (
+	"context"
+
+	ssov1 "github.com/baimz/protos/gen/go/sso"
+	"google.golang.org/grpc"
+)
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
+}
+
+func Register(gRPC *grpc.Server) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+}
+
+func (s *serverAPI) Login(
+	context.Context,
+	*ssov1.LoginRequest,
+) (*ssov1.LoginResponse, error) {
+	panic("implement me")
+}
+func (s *serverAPI) IsAdmin(
+	ctx context.Context,
+	req *ssov1.IsAdminRequest,
+) (*ssov1.IsAdminResponse, error) {
+	panic("implement me")
+}
+func (s *serverAPI) Login(
+	context.Context,
+	**ssov1.LoginRequest,
+) (*ssov1.LoginResponse, error) {
+	panic("implement me")
+}
+func (s *serverAPI) IsAdmin(
+	context.Context,
+	*ssov1.IsAdminRequest,
+) (*ssov1.IsAdminResponse, error) {
+	panic("implement me")
 }
